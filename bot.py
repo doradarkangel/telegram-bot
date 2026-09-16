c6bcc5be4d969e56adf3235d4bcfb479178bc3b6
doradarkangel <doradarkangel1301@gmail.com>
2026-09-16 20:59:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ THREAD_SIGA = 43559
 THREAD_NESSA = 80703
 THREAD_FENIX = 104708
 THREAD_DANTON = 108032
+THREAD_RAYZER = 116418
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
@@ -175,6 +176,8 @@ async def forward_to_group(message: types.Message):
         target_thread = THREAD_FENIX
     elif "#дантон" in text_lower:
         target_thread = THREAD_DANTON
+    elif "#рейзер" in text_lower:
+        target_thread = THREAD_RAYZER
 
     # Проверяем прошлый тег из базы данных, если в текущем сообщении тега нет
     if not target_thread:
